3c8f1e2b83fb2032e0847e6b7924ed036d835f74
root <root@localhost.localdomain>
2016-09-06 11:42:13 +0800

    change to mysql

--- simplecmdb/simplecmdb/settings.py
+++ simplecmdb/simplecmdb/settings.py
@@ -56,11 +56,21 @@ WSGI_APPLICATION = 'simplecmdb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+DATABASES = { 
+    'default': { 
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cmdb',
+        'USER': 'dang',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306', 
+    } 
 }
 
 # Internationalization
